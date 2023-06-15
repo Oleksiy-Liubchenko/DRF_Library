9df84008a8fd3672f125fc6f834ad4d93da626ba
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-06-15 19:07:07 +0300

    Add Borrowing app with model

---
+++ borrowings/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from DRF_Library import settings
+from books.models import Books
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=False)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
