4f2c7ab10441e13a4f176245da9a2c2495c9cc56
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-06-15 15:17:07 +0300

    Add register View for User &  View list of all users for staff

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from user.views import ManagingUser, CreateUserView, UsersListView
+
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManagingUser.as_view(), name="my_user"),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("all_users/", UsersListView.as_view(), name="users"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from user.serizlizers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Creating new user with *email, *password, first_name, last_name"""
+    serializer_class = UserSerializer
+
+
+class ManagingUser(generics.RetrieveUpdateAPIView):
+    """User profile with managing info opportunity"""
+
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class UsersListView(APIView):
+    """List of all users for users who have is_staff = True"""
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if not request.user.is_staff:
+            return Response(
+                {"detail": "User is not staff"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        users = get_user_model().objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
